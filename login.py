ca7af16c4e8bcae29f31152bc773c3cf37fa533a
theotakuduniya <theotakuduniya@gmail.com>
2024-08-05 08:19:58 +0530

    Update login.py

--- login.py
+++ login.py
@@ -5,8 +5,8 @@ try:
     from config import API_HASH, API_ID
 except Exception:
     print("Failed load API_HASH and API_ID from .env")
-    API_HASH = input("Input telegram API_HASH:6c0ea77e38c2286a9d4d7b1bd0f07bd5 ")
-    API_ID = input("Input telegram API_ID:21778536")
+    API_HASH = input("Input telegram API_HASH:")
+    API_ID = input("Input telegram API_ID:")
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
